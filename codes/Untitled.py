a208ae2fa9f54d8274fe2107720d6c467e4c303c
GitHimanshuc <unphys@gmail.com>
2020-06-14 02:36:04 +0530

    Testing the hooks

---
+++ codes/Untitled.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
+import seaborn as sns
+import numpy as np
+import pandas as pd
+print(plt.style.available)
+
+
+# In[2]:
+
+
+1+1
+
+
+# In[ ]:
+
+
+
+
